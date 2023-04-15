1a034d65015c061962eb63aa7bb205ab38b45816
Natalyaprism <130831642+Natalyaprism@users.noreply.github.com>
2023-04-15 12:48:17 +0300

    Add files via upload

---
+++ Zad 10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10
+//Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает вторую цифру этого числа
+//                               456 -> 5
+//                               782 -> 8
+//                               918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int threeDigitNumber= Convert.ToInt32(Console.ReadLine());
+String StringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра числа = " +StringNumber[1]);
---
+++ Zad 13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// Или сообщает что третьего числа нет.)
+
+Console.WriteLine("Введите число");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+String anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length>2)
+{
+ Console.WriteLine("Третья цифра "+anyNumberText[2]);   
+}
+else
+{
+    Console.WriteLine("Нет цифры");
+}
